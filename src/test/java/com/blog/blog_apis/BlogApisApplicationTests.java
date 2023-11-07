cf840ebcffced392c033969da8a8a7f196cb3ea7
Saad Ur Rehman Baig <saad.rehman@matrix.com.pk>
2023-11-07 17:56:54 +0500

    first commit

---
+++ src/test/java/com/blog/blog_apis/BlogApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blog_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
