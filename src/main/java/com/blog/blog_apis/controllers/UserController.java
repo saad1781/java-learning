10ae6a67b6654bb71325940744fce6a9b14d1a2d
Saad Ur Rehman Baig <saad.rehman@matrix.com.pk>
2023-11-13 18:59:29 +0500

    feat/update user api done

--- src/main/java/com/blog/blog_apis/controllers/UserController.java
+++ src/main/java/com/blog/blog_apis/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.blog.blog_apis.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blog.blog_apis.payloads.ApiResponse;
 import com.blog.blog_apis.payloads.UserDto;
 import com.blog.blog_apis.services.UserService;
 
@@ -18,11 +23,20 @@ public class UserController {
 
     // post
     @PostMapping("/")
-    public UserDto createUser(@RequestBody UserDto userDto){
-        return this.userService.createUser(userDto);
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        return new ResponseEntity<>(this.userService.createUser(userDto), HttpStatus.CREATED);
     }
 
     // put -update
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<ApiResponse<UserDto>> updateUser(@RequestBody UserDto userDto,
+            @PathVariable("userId") int userid) {
+                System.out.println(userid);
+                System.out.println("\n\n\n\n\n");
+        return new ResponseEntity<ApiResponse<UserDto>>(new ApiResponse<UserDto>("user Update successfully", true,
+                this.userService.updateUser(userDto, userid)), HttpStatus.OK);
+    }
 
     // delete- delete
 
