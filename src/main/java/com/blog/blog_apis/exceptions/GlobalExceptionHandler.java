11d2a5c90e8e0f97cd53a4a0a7bfa602e695c4c2
Saad Ur Rehman Baig <saad.rehman@matrix.com.pk>
2023-11-14 17:22:10 +0500

    fix/ removed unused dependency

--- src/main/java/com/blog/blog_apis/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blog/blog_apis/exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.blog.blog_apis.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
