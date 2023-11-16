1d0a033eff9e56afd8fc26c8819bb87d344779eb
Saad Ur Rehman Baig <saad.rehman@matrix.com.pk>
2023-11-16 13:51:04 +0500

    feat/validation added in catagory

--- src/main/java/com/blog/blog_apis/controllers/CatagoryController.java
+++ src/main/java/com/blog/blog_apis/controllers/CatagoryController.java
@@ -22,45 +22,45 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/catagory")
 public class CatagoryController {
-    @Autowired
-    private CatogoryService catagoryService;
+        @Autowired
+        private CatogoryService catagoryService;
 
-    // post
-    @PostMapping("/")
-    public ResponseEntity<CatagoryDto> createUser(@Valid @RequestBody CatagoryDto catagoryDto) {
-        return new ResponseEntity<CatagoryDto>(this.catagoryService.create(catagoryDto), HttpStatus.CREATED);
-    }
+        // post
+        @PostMapping("/")
+        public ResponseEntity<CatagoryDto> createUser(@Valid @RequestBody CatagoryDto catagoryDto) {
+                return new ResponseEntity<CatagoryDto>(this.catagoryService.create(catagoryDto), HttpStatus.CREATED);
+        }
 
-    // delete- delete
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Void>> deleteCatagory(@PathVariable int id) {
-        this.catagoryService.delete(id);
-        return new ResponseEntity<>(new ApiResponse<>("Catagory Deleted successfully", true), HttpStatus.OK);
-    }
+        // delete- delete
+        @DeleteMapping("/{id}")
+        public ResponseEntity<ApiResponse<Void>> deleteCatagory(@PathVariable int id) {
+                this.catagoryService.delete(id);
+                return new ResponseEntity<>(new ApiResponse<>("Catagory Deleted successfully", true), HttpStatus.OK);
+        }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<CatagoryDto>> updateCatagory(@RequestBody CatagoryDto catagoryDto,
-            @PathVariable("id") int id) {
-        catagoryDto.setId(id);
-        return new ResponseEntity<ApiResponse<CatagoryDto>>(
-                new ApiResponse<CatagoryDto>("user Update successfully", true,
-                        this.catagoryService.update(catagoryDto, id)),
-                HttpStatus.OK);
-    }
+        @PutMapping("/{id}")
+        public ResponseEntity<ApiResponse<CatagoryDto>> updateCatagory(@Valid() @RequestBody CatagoryDto catagoryDto,
+                        @PathVariable("id") int id) {
+                catagoryDto.setId(id);
+                return new ResponseEntity<ApiResponse<CatagoryDto>>(
+                                new ApiResponse<CatagoryDto>("user Update successfully", true,
+                                                this.catagoryService.update(catagoryDto, id)),
+                                HttpStatus.OK);
+        }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<CatagoryDto>> getCatagoryById(@PathVariable Integer id) {
-        return new ResponseEntity<ApiResponse<CatagoryDto>>(
-                new ApiResponse<CatagoryDto>("Catagory Fetched Successfully", true,
-                        this.catagoryService.getById(id)),
-                HttpStatus.OK);
-    }
+        @GetMapping("/{id}")
+        public ResponseEntity<ApiResponse<CatagoryDto>> getCatagoryById(@PathVariable Integer id) {
+                return new ResponseEntity<ApiResponse<CatagoryDto>>(
+                                new ApiResponse<CatagoryDto>("Catagory Fetched Successfully", true,
+                                                this.catagoryService.getById(id)),
+                                HttpStatus.OK);
+        }
 
-   @GetMapping("/")
-   public ResponseEntity<ApiResponse<List<CatagoryDto>>> getAllCatagories() {
-        return new ResponseEntity<ApiResponse<List<CatagoryDto>>>(
-                new ApiResponse<List<CatagoryDto>>("Catagories Fetched Successfully", true,
-                        this.catagoryService.getAll()),
-                HttpStatus.OK);
-    }
+        @GetMapping("/")
+        public ResponseEntity<ApiResponse<List<CatagoryDto>>> getAllCatagories() {
+                return new ResponseEntity<ApiResponse<List<CatagoryDto>>>(
+                                new ApiResponse<List<CatagoryDto>>("Catagories Fetched Successfully", true,
+                                                this.catagoryService.getAll()),
+                                HttpStatus.OK);
+        }
 }
--- src/main/java/com/blog/blog_apis/payloads/CatagoryDto.java
+++ src/main/java/com/blog/blog_apis/payloads/CatagoryDto.java
@@ -1,5 +1,7 @@
 package com.blog.blog_apis.payloads;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CatagoryDto {
+    @NotBlank()
+    @Size(min = 4)
     private String title;
+
+    @NotBlank()
+    @Size(min = 10)
     private String description;
     private int id;
 }
